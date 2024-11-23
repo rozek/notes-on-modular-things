a7a8cc0bd3c0e227ec51990dd83d938f8447fbff
Andreas Rozek <A.Rozek@gmx.de>
2024-11-23 19:19:04 +0100

    Create xxx.ts

---
+++ xxx/software/xxx.ts
@@ -0,0 +1,11 @@
+import Thing from "../../../src/lib/thing"
+
+export default class xxx extends Thing {  <<<< don't forget to insert your thing's name here
+<<<< add any required methods here
+
+/**** API Specification ****/
+
+  public api = [
+<<<< add the interface documentation here
+  ]
+}
